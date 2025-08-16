5f6212b2c6d1e765fce9e34887321996927f6448
krishnendu127 <your.email@example.com>
2025-08-17 03:20:40 +0530

    added controller

---
+++ Order/src/main/java/com/KB/Order/Controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.KB.Order.Controller;
+
+import com.KB.Order.DTO.OrderRequest;
+import com.KB.Order.Service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping("/placeOrder")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
